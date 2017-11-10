5148debcbac5bb44aaeefa836e3d79d773f7d130
frankybo57 <32872685+frankybo57@users.noreply.github.com>
2017-11-10 07:55:16 +0100

    Noyau - model
    Ajout de la classe Utilisateur

---
+++ monProjet/Noyau/src/main/java/noyau/model/Utilisateur.java
@@ -0,0 +1,133 @@
+package noyau.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+@SequenceGenerator(name = "sequenceUtilisateur", sequenceName = "SequenceGenerator")
+public class Utilisateur {
+	@Id @GeneratedValue(generator = "sequenceUtilisateur")
+	private Integer id;
+	
+	private String login;
+	
+	private String password;
+	
+	private Droit droits;
+	
+	public Utilisateur(){
+		
+	}
+	
+	public Utilisateur(String login, String password){
+		
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the login
+	 */
+	public String getLogin() {
+		return login;
+	}
+
+	/**
+	 * @param login the login to set
+	 */
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the droits
+	 */
+	public Droit getDroits() {
+		return droits;
+	}
+
+	/**
+	 * @param droits the droits to set
+	 */
+	public void setDroits(Droit droits) {
+		this.droits = droits;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((droits == null) ? 0 : droits.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((login == null) ? 0 : login.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Utilisateur other = (Utilisateur) obj;
+		if (droits != other.droits)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (login == null) {
+			if (other.login != null)
+				return false;
+		} else if (!login.equals(other.login))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		return true;
+	}
+	
+	
+}
