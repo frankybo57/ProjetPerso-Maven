bbe4b39ed4173612cb7099492a4d1d1e23e34e5a
frankybo57 <32872685+frankybo57@users.noreply.github.com>
2017-11-10 05:08:19 +0100

    Ajout module maven - ModuleComptes

---
+++ monProjet/ModuleComptes/src/main/java/org/ModuleComptes/App.java
@@ -0,0 +1,13 @@
+package org.ModuleComptes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
