dffcd168f57bf1dcd3891dbadbe42039a04cf0ea
frankybo57 <32872685+frankybo57@users.noreply.github.com>
2017-11-10 08:04:18 +0100

    Noyau - model
    Ajout d'une classe Module

---
+++ monProjet/Noyau/src/main/java/noyau/model/Module.java
@@ -0,0 +1,111 @@
+package noyau.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "modules")
+@SequenceGenerator(name = "sequenceModule", sequenceName = "SequenceModule")
+public class Module {
+	@Id @GeneratedValue(generator = "sequenceUtilisateur")
+	private Integer id;
+	@Column(name = "name")
+	private String nom;
+	@Column(name = "state")
+	private Etat etat;
+	
+	public Module(){
+		
+	}
+	
+	public Module(String nom){
+		this.nom = nom;
+		this.etat = Etat.Inactif;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	/**
+	 * @return the etat
+	 */
+	public Etat getEtat() {
+		return etat;
+	}
+
+	/**
+	 * @param etat the etat to set
+	 */
+	public void setEtat(Etat etat) {
+		this.etat = etat;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((etat == null) ? 0 : etat.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nom == null) ? 0 : nom.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Module other = (Module) obj;
+		if (etat != other.etat)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nom == null) {
+			if (other.nom != null)
+				return false;
+		} else if (!nom.equals(other.nom))
+			return false;
+		return true;
+	}
+}
